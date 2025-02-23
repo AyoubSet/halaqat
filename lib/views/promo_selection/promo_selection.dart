88b12580bb6804ce20910e6825654ba72ec68fdc
AyoubSet <ayoubset2005@gmail.com>
2025-02-23 22:04:07 +0100

    Initial Commit

---
+++ lib/views/promo_selection/promo_selection.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+//This is where the user choose the promo
+class PromoSelectionView extends StatelessWidget {
+  const PromoSelectionView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
